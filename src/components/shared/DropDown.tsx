926f468955fd6db6979f8b4af66c44ca985829dd
SUHONG\topic <topic828@Naver.com>
2024-03-13 00:55:01 +0900

    1차드롭다운 css 작업예쩡

--- src/components/shared/DropDown.tsx
+++ src/components/shared/DropDown.tsx
@@ -1,30 +1,98 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './DropDown.css';
 import { DropDownData } from '../constant/data';
 
 // 제네릭으로 타입 넘길것!
 const DropDown: React.FC<{ list: DropDownData[] }> = ({ list }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [dropValue, setDropValue] = useState<string | undefined>();
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
   const onOpenHandler = () => {
     setIsOpen(!isOpen);
   };
 
-  const [dropValue, setDropValue] = useState<string | undefined>();
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (isOpen) {
+      switch (e.key) {
+        case 'ArrowUp':
+          e.preventDefault();
+          setSelectedIndex((prevIndex) =>
+            prevIndex === null
+              ? list.length - 1
+              : (prevIndex - 1 + list.length) % list.length,
+          );
+          break;
+        case 'ArrowDown':
+          e.preventDefault();
+          setSelectedIndex((prevIndex) =>
+            prevIndex === null ? 0 : (prevIndex + 1) % list.length,
+          );
+          break;
+        default:
+          break;
+      }
+    }
+  };
 
-  const dropHeaderClass = `dropHeader ${isOpen ? 'open' : ''}`;
+  const handleDocumentClick = (event: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node)
+    ) {
+      setIsOpen(false);
+      setSelectedIndex(null);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleDocumentClick);
+
+    return () => {
+      document.removeEventListener('click', handleDocumentClick);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (selectedIndex !== null) {
+      setDropValue(list[selectedIndex].value);
+    } else {
+      setDropValue(undefined);
+    }
+  }, [selectedIndex, list]);
 
   return (
     <>
       {/* header */}
-      <div className={dropHeaderClass} onClick={onOpenHandler}>
+      <div
+        ref={dropdownRef}
+        className={`dropHeader ${isOpen ? 'open' : ''}`}
+        onClick={onOpenHandler}
+        onKeyDown={handleKeyDown}
+        tabIndex={0}
+      >
         <span>{dropValue ? dropValue : '선택'}</span>
         <div className="dropIcon">{isOpen ? '⌃' : '⌄'}</div>
       </div>
       {isOpen &&
-        list.map(({ id, value, label }) => (
+        list.map(({ id, value, label }, index) => (
           <React.Fragment key={id}>
-            <label>{label}</label>
-            <div>{value}</div>
+            <label
+              className={`dropdown-label ${
+                index === selectedIndex ? 'selected' : ''
+              }`}
+            >
+              {label}
+            </label>
+            <div
+              className={`dropdown-value ${
+                index === selectedIndex ? 'selected' : ''
+              }`}
+            >
+              {value}
+            </div>
           </React.Fragment>
         ))}
     </>
